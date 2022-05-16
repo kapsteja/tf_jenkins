f6c589b4b4211739afcb5edc7581adb18ef72ea4
kapsteja <93643561+kapsteja@users.noreply.github.com>
2022-05-16 18:06:52 +0530

    Update vpc.tf

--- vpc.tf
+++ vpc.tf
@@ -1,6 +1,6 @@
 provider "aws" {
-   access_key = "AKIAWIMX3MIVEQPOPM4H"
-   secret_key = "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"
+   access_key = "*******"
+   secret_key = "*******"
     region = "us-east-1"
 }
 resource "aws_vpc" "main" {
